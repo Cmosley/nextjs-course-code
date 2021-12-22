bf1f1041fb849e4ed36fcb32672c9a9329ca002b
cmosley <cmosley@beholdmedia.com>
2021-12-21 23:52:16 -0600

    meetup detail html/css

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 function MeetupDetails() {
   return (
-    <>
-    <img src="" alt="" />
-    </>
-  )
+    <MeetupDetail 
+    image="https://images.unsplash.com/photo-1542137722061-efd1cbdf156c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" 
+    title="First Meetup" 
+    address="Some Street 5, Some City" 
+    description="This is first meeetup"
+    />
+  );
 }
 
 
